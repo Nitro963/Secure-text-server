9a07529f41029eedfa0fce109ab11a85179527ba
unknown <eng.osama1998@gmail.com>
2020-11-27 21:00:13 +0200

    client abstraction

--- test_client.py
+++ test_client.py
@@ -2,5 +2,5 @@ import asyncio
 from client.manager import start_client, request
 
 if __name__ == '__main__':
-    # asyncio.run(start_client())
-    asyncio.run(request('127.0.0.1', 8888, 'text_file_name', 'view'))
+    #asyncio.run(start_client())
+    asyncio.run(request('127.0.0.1', 8888, 'text_file_name', 'message'))
