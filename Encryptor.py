30a48c0e6f1c2c3d5ad1385e7610a3dc18204e61
ammar0ah <ammar0ah@gmail.com>
2020-11-30 20:52:52 +0200

    fetching

--- Encryptor.py
+++ Encryptor.py
@@ -1,6 +1,7 @@
 from Crypto.Cipher import AES
 from Crypto.Util.Padding import pad, unpad
 from Crypto import Random
+from Crypto.PublicKey import RSA
 
 
 class Encryptor:
@@ -11,6 +12,11 @@ class Encryptor:
 
     def encrypt_message(self, message: str,iv):
         print('message',message)
+    @staticmethod
+    def generate_iv():
+        return Random.new().read(AES.block_size)
+
+    def encrypt_message(self, message: bytes, iv):
         try:
             cipher = AES.new(self.key, AES.MODE_CBC, iv)
             ciphertext = cipher.encrypt(pad(message, AES.block_size))
@@ -22,3 +28,16 @@ class Encryptor:
         cipher = AES.new(self.key, AES.MODE_CBC, iv)
         decrypted_text = unpad(cipher.decrypt(data), AES.block_size).decode()
         return decrypted_text
+    def decrypt_message(self, data: bytes, iv):
+        cipher = AES.new(self.key, AES.MODE_CBC, iv)
+        decrypted_data = unpad(cipher.decrypt(data), AES.block_size)
+        return decrypted_data
+
+    @staticmethod
+    def generate_key_pairs():
+        key = RSA.generate(2048)
+        private_key = key.export_key()
+
+        public_key = key.publickey().export_key()
+
+        return private_key, public_key
